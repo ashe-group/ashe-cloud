9fce5b9ea5e5a9b5c0adb46857206205a0dfacac
ashe_business@sina.com <2*o8eV0@xA8XLI%pnTgD>
2018-11-16 17:01:21 +0800

    init

---
+++ ashe-cloud-component-monitor-server/src/main/java/com/ashe/cloud/component/monitor/server/configuration/EnableMonitorServer.java
@@ -0,0 +1,15 @@
+/*!
+ * ash cloud.
+ * https://www.ashe.com/
+ */
+package com.ashe.cloud.component.monitor.server.configuration;
+
+/**
+ * 启用监控服务端
+ *
+ * @author 刘斌华
+ * @since 1.0.0
+ */
+public @interface EnableMonitorServer {
+	
+}
---
+++ ashe-cloud-component-monitor-server/src/main/java/com/ashe/cloud/component/monitor/server/instance/ClientDiscovery.java
@@ -0,0 +1,15 @@
+/*!
+ * ash cloud.
+ * https://www.ashe.com/
+ */
+package com.ashe.cloud.component.monitor.server.instance;
+
+/**
+ * 发现实例
+ *
+ * @author 刘斌华
+ * @since 1.0.0
+ */
+public class ClientDiscovery {
+	
+}
